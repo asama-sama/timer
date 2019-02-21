70fc0609adc72a2dac98b521a7edc33f4e39ff12
Asama Qureshi <asama.qureshi@gmail.com>
2019-02-22 08:51:34 +1100

    refactor: timerlist into presentational component

--- TimerListComponent/TimerList.jsx
+++ TimerListComponent/TimerList.jsx
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'semantic-ui-react';
 import './TimerList.css';
 
-class TimerList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        {this.props.timers.map(timer => 
-          <div key={timer.name}>
-            <Icon 
-              name='unhide'
-              styleName='TimerList-Unhide' 
-              onClick={()=>this.props.unhideTimer(timer.id, this.props.date)}
-            />
-            <span>{timer.name}</span>
-          </div>)}
-      </div>
-    );
-  }
-}
+const TimerList = ({timers, unhideTimer, date}) => (
+  <div>
+    {timers.map(timer => 
+      <div key={timer.name}>
+        <Icon 
+          name='unhide'
+          styleName='TimerList-Unhide' 
+          onClick={()=>unhideTimer(timer.id, date)}
+        />
+        <span>{timer.name}</span>
+      </div>)}
+  </div>
+);
 TimerList.propTypes = {
   timers: PropTypes.array.isRequired,
   unhideTimer: PropTypes.func.isRequired,
