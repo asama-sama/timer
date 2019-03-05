d5b6e48191c6ea778d831ba48caaca45807e331d
Asama Qureshi <asama.qureshi@gmail.com>
2019-03-05 17:22:47 +1100

    style: added some padding to top

--- src/indexDom.jsx
+++ src/indexDom.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TimerAppWrapper from './TimerAppWrapper';
-ReactDOM.render(<TimerAppWrapper timerData={{}} />, 
+ReactDOM.render(<div style={{paddingTop: '20px'}}><TimerAppWrapper timerData={{}} /></div>, 
   document.getElementById('app'));
