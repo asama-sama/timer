c876e9ba4e86092e9c7963dd00aa90d8d735f25c
Asama Qureshi <asama.qureshi@gmail.com>
2019-03-07 15:51:32 +1100

    style: updated according to linter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,9 @@
 import moment from 'moment';
-import {combineReducers} from 'redux';
-import { createStore, applyMiddleware } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import uuid from 'uuid-random';
-import {isTimerActiveForDate} from '../utils';
+import { isTimerActiveForDate } from '../utils';
 
 const timer = (
   state = {
@@ -12,118 +11,115 @@ const timer = (
     timeBlocks: [],
     visibleDates: []
   },
-  action) => {
-
-  switch(action.type) {
-  case 'START_TIMER':{
-    // end all running timers, on any timer
-    let timeBlocks = state.timeBlocks.map(tb => {
-      if(tb.end === undefined) {
-        tb.end = moment().format();
-      }
-      return tb;
-    });
-    let newState = {
-      ...state, timeBlocks: [...timeBlocks]
-    };
-    if (state.id === action.id) {
-      newState = {
-        ...state,
-        timeBlocks: [
-          ...timeBlocks,
-          {
-            id: uuid(),
-            start: moment().format()
-          }
-        ]};
-    }
-    return newState;
-  }
-  case 'STOP_TIMER':{
-    if(state.id === action.id) {
-      let timeBlocks = state.timeBlocks.map(tb => {
-        if(tb.end === undefined) {
-          tb.end = moment().format();
+  action
+) => {
+  switch (action.type) {
+    case 'START_TIMER': {
+      // end all running timers, on any timer
+      const timeBlocks = state.timeBlocks.map(tb => {
+        const ntb = { ...tb };
+        if (tb.end === undefined) {
+          ntb.end = moment().format();
         }
-        return tb;
+        return ntb;
       });
-      return {...state, timeBlocks};
-    } else {
-      return state;
-    }
-  }
-  case 'HIDE_TIMER':{
-    let timerActive = isTimerActiveForDate(state, action.date);
-    let visibleDates =
-      state.visibleDates.filter(d => {
-        return !moment(d).isSame(
-          moment(action.date), 'day');
-      });
-    if(action.id === state.id && !timerActive) {
-      return {...state, visibleDates};
-    } else {
-      return state;
-    }
-  }
-  case 'UNHIDE_TIMER':
-    if(action.id===state.id) {
-      let visibleDates = [
-        ...state.visibleDates,
-        moment(action.date).startOf('day').format()
-      ];
-      return {...state, visibleDates};
-    } else {
-      return state;
-    }
-  case 'UNHIDE_RUNNING_TIMERS_FOR_DATE': {
-    let running = state.timeBlocks.map(tb => {
-      return !tb.end;
-    }).reduce((acc, next) => acc || next, false);
-    let visibleDates = [...state.visibleDates];
-    if(running) {
-      let date = moment(action.date);
-      let isVisible = visibleDates
-        .find(vd => moment(vd).isSame(date, 'day'));
-      if(!isVisible) {
-        visibleDates = [
-          ...visibleDates,
-          date.startOf('day').format()
-        ];
+      let newState = {
+        ...state, timeBlocks: [...timeBlocks]
+      };
+      if (state.id === action.id) {
+        newState = {
+          ...state,
+          timeBlocks: [
+            ...timeBlocks,
+            {
+              id: uuid(),
+              start: moment().format()
+            }
+          ]
+        };
       }
+      return newState;
     }
-    return {...state, visibleDates};
-  }
-  case 'DELETE_TIME_BLOCK': {
-    let timeBlocks = state.timeBlocks.filter(tb => tb.id !== action.timeBlockId);
-    return {...state, timeBlocks};
-  }
-  case 'UPDATE_TIME_BLOCK_START':
-    return {
-      ...state,
-      timeBlocks:
+    case 'STOP_TIMER': {
+      if (state.id === action.id) {
+        const timeBlocks = state.timeBlocks.map(tb => {
+          const ntb = { ...tb };
+          if (tb.end === undefined) {
+            ntb.end = moment().format();
+          }
+          return ntb;
+        });
+        return { ...state, timeBlocks };
+      }
+      return state;
+    }
+    case 'HIDE_TIMER': {
+      const timerActive = isTimerActiveForDate(state, action.date);
+      const visibleDates = state.visibleDates.filter(d => (
+        !moment(d).isSame(moment(action.date), 'day')));
+      if (action.id === state.id && !timerActive) {
+        return { ...state, visibleDates };
+      }
+      return state;
+    }
+    case 'UNHIDE_TIMER':
+      if (action.id === state.id) {
+        const visibleDates = [
+          ...state.visibleDates,
+          moment(action.date).startOf('day').format()
+        ];
+        return { ...state, visibleDates };
+      }
+      return state;
+    case 'UNHIDE_RUNNING_TIMERS_FOR_DATE': {
+      const running = state.timeBlocks.map(tb => (
+        !tb.end
+      )).reduce((acc, next) => acc || next, false);
+      let visibleDates = [...state.visibleDates];
+      if (running) {
+        const date = moment(action.date);
+        const isVisible = visibleDates
+          .find(vd => moment(vd).isSame(date, 'day'));
+        if (!isVisible) {
+          visibleDates = [
+            ...visibleDates,
+            date.startOf('day').format()
+          ];
+        }
+      }
+      return { ...state, visibleDates };
+    }
+    case 'DELETE_TIME_BLOCK': {
+      const timeBlocks = state.timeBlocks.filter(tb => tb.id !== action.timeBlockId);
+      return { ...state, timeBlocks };
+    }
+    case 'UPDATE_TIME_BLOCK_START':
+      return {
+        ...state,
+        timeBlocks:
+          state.timeBlocks.map(tb => (
+            tb.id === action.id
+              ? {
+                ...tb,
+                start: action.time
+              }
+              : tb))
+      };
+    case 'UPDATE_TIME_BLOCK_END': {
+      return {
+        ...state,
+        timeBlocks:
         state.timeBlocks.map(tb => (
-          tb.id === action.id ?
-            {
-              ...tb,
-              start: action.time
-            } 
-            : tb ))
-    };
-  case 'UPDATE_TIME_BLOCK_END': {
-    return {
-      ...state,
-      timeBlocks:
-        state.timeBlocks.map(tb => (
-          tb.id === action.id ?
-            {
+          tb.id === action.id
+            ? {
               ...tb,
               end: action.time
             }
             : tb))
-    };
-  }
-  default:
-    return state;
+      };
+    }
+    default:
+      return state;
   }
 };
 
@@ -134,47 +130,48 @@ const timers = (
     timersState: {
       items: []
     },
-  }, action) => {
-  switch(action.type) {
-  case 'REQUEST_STATE': 
-    return {
-      ...state,
-      isFetching: true,
-      didInvalidate: false
-    };
-  case 'RECEIVE_STATE' : {
-    let newState = {
-      ...state,
-      isFetching: false,
-      didInvalidate: false,
-      lastUpdated: action.receivedAt
-    };
-    if(action.data !== undefined) {
-      newState = {
-        ...newState,
+  }, action
+) => {
+  switch (action.type) {
+    case 'REQUEST_STATE':
+      return {
+        ...state,
+        isFetching: true,
+        didInvalidate: false
+      };
+    case 'RECEIVE_STATE': {
+      let newState = {
+        ...state,
+        isFetching: false,
+        didInvalidate: false,
+        lastUpdated: action.receivedAt
+      };
+      if (action.data !== undefined) {
+        newState = {
+          ...newState,
+          timersState: action.data
+        };
+      }
+      return newState;
+    }
+    case 'UPDATE_STATE': {
+      return {
+        ...state,
+        isFetching: false,
+        didInvalidate: false,
+        lastUpdated: action.receivedAt,
         timersState: action.data
       };
     }
-    return newState;
-  }
-  case 'UPDATE_STATE' : {
-    return {
-      ...state,
-      isFetching: false,
-      didInvalidate: false,
-      lastUpdated: action.receivedAt,
-      timersState: action.data
-    };
-  }
-  case 'ADD_TIMER': {
-    let newTimerInput = action.name.trim();
-    if(state.timersState.items.find(timer => timer.name === newTimerInput)) {
-      console.error(`Timer with name ${newTimerInput} already exists`);
-      return state;
-    }
-    if(newTimerInput === '') {
-      return state;
-    } else {
+    case 'ADD_TIMER': {
+      const newTimerInput = action.name.trim();
+      if (state.timersState.items.find(t => t.name === newTimerInput)) {
+        console.error(`Timer with name ${newTimerInput} already exists`);
+        return state;
+      }
+      if (newTimerInput === '') {
+        return state;
+      }
       return {
         ...state,
         timersState: {
@@ -190,42 +187,42 @@ const timers = (
         }
       };
     }
-  }
 
-  case 'START_TIMER':
-  case 'STOP_TIMER':
-  case 'HIDE_TIMER':
-  case 'UNHIDE_TIMER':
-  case 'UNHIDE_RUNNING_TIMERS_FOR_DATE':
-  case 'UNHIDE_TIMERS':
-  case 'DELETE_TIME_BLOCK':
-  case 'UPDATE_TIME_BLOCK_START':
-  case 'UPDATE_TIME_BLOCK_END': {
-    let items = state.timersState.items.map(t => timer(t, action));
-    return {
-      ...state,
-      timersState: {
-        ...state.timersState,
-        items
-      }
-    };
-  }
-  default:
-    return state;
+    case 'START_TIMER':
+    case 'STOP_TIMER':
+    case 'HIDE_TIMER':
+    case 'UNHIDE_TIMER':
+    case 'UNHIDE_RUNNING_TIMERS_FOR_DATE':
+    case 'UNHIDE_TIMERS':
+    case 'DELETE_TIME_BLOCK':
+    case 'UPDATE_TIME_BLOCK_START':
+    case 'UPDATE_TIME_BLOCK_END': {
+      const items = state.timersState.items.map(t => timer(t, action));
+      return {
+        ...state,
+        timersState: {
+          ...state.timersState,
+          items
+        }
+      };
+    }
+    default:
+      return state;
   }
 };
 
 const date = (
-  state = moment().startOf('day').format(), 
-  action) => {
-  let date = moment(state);
-  switch(action.type){
-  case 'DAY_FORWARD':
-    return moment(date).add(1, 'd').format();
-  case 'DAY_BACK':
-    return moment(date).subtract(1, 'd').format();
-  default:
-    return state;
+  state = moment().startOf('day').format(),
+  action
+) => {
+  const dt = moment(state);
+  switch (action.type) {
+    case 'DAY_FORWARD':
+      return moment(dt).add(1, 'd').format();
+    case 'DAY_BACK':
+      return moment(dt).subtract(1, 'd').format();
+    default:
+      return state;
   }
 };
 
@@ -240,7 +237,8 @@ const store = createStore(
   rootReducer,
   applyMiddleware(
     thunkMiddleware,
-    loggerMiddleware)
+    loggerMiddleware
+  )
 );
 
 export default store;
