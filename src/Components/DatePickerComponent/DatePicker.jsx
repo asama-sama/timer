9d41fc4ca0867f37efc1828d2c019acd82bcda9e
Asama Qureshi <asama.qureshi@gmail.com>
2019-03-08 12:42:12 +1100

    style: edited code to be in line with linter

--- src/Components/DatePickerComponent/DatePicker.jsx
+++ src/Components/DatePickerComponent/DatePicker.jsx
@@ -1,33 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'semantic-ui-react';
 import './DatePicker.css';
 import moment from 'moment';
 
-class DatePicker extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div styleName='DatePicker'>
-        <Icon 
-          name='angle left'
-          styleName='DatePicker-Arrow DatePicker-Arrow-Left'
-          onClick={() => this.props.subtractDay()} />
-        <span styleName='DatePicker-Text'>
-          {moment(this.props.date).format('ddd Do MMM YYYY')}
-        </span>
-        <Icon 
-          name='angle right' 
-          styleName='DatePicker-Arrow DatePicker-Arrow-Right'
-          onClick={() => this.props.addDay()} />
-      </div>);
-  }
-}
+const DatePicker = props => {
+  const { subtractDay, date, addDay } = props;
+  return (
+    <div styleName='DatePicker'>
+      <Icon
+        name='angle left'
+        styleName='DatePicker-Arrow DatePicker-Arrow-Left'
+        onClick={() => subtractDay()}
+      />
+      <span styleName='DatePicker-Text'>
+        {moment(date).format('ddd Do MMM YYYY')}
+      </span>
+      <Icon
+        name='angle right'
+        styleName='DatePicker-Arrow DatePicker-Arrow-Right'
+        onClick={() => addDay()}
+      />
+    </div>
+  );
+};
 DatePicker.propTypes = {
-  date: PropTypes.any.isRequired,
+  date: PropTypes.string.isRequired,
   subtractDay: PropTypes.func.isRequired,
   addDay: PropTypes.func.isRequired
 };
